4bf11c8165997f1b0894e7d3d158b886ff292c44
Felipe Feitosa da Silva <119868281+FelipeFeitosa97@users.noreply.github.com>
2023-06-13 12:44:33 -0300

    teste commit

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,8 @@
   Route::post('login', [AuthController::class, 'login']);
   Route::post('register', [AuthController::class, 'register']);
 
+
+  
   Route::group(['middleware' => 'auth:sanctum'], function() {
     Route::get('logout', [AuthController::class, 'logout']);
     Route::get('user', [AuthController::class, 'user']);
